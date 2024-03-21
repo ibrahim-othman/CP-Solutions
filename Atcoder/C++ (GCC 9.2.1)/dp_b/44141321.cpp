92138a08766cda6a4ae4d22783927b33be6d8c60
إبراهيم <93207426+othmaine@users.noreply.github.com>
2024-03-21 08:37:36 +0200

    Committing files

---
+++ Atcoder/C++ (GCC 9.2.1)/dp_b/44141321.cpp
@@ -0,0 +1,90 @@
+#include <bits/stdc++.h>
+#define loop(i, n) for(ll i = 0; i< n; i++)
+#define Loop(i, n) for(ll i = 1; i< n; i++)
+#define ll long long
+#define fillArray(n, arr) ll arr[n]; for(ll i=0; i< n; i++) cin>>arr[i]
+#define fillVector(n, arr) vector<ll> arr(n);loop(i,n)cin>>arr[i];
+#define speed cin.tie(0); cin.sync_with_stdio(0)
+#define tests  ll t;cin>>t;Loop(T,t+1)
+#define OO 1223372036854775807//9223372036854775807
+#define dotProduct(a,b) ((conj(a)*(b)).real())
+#define crossProduct(a,b) ((conj(a)*b).imag())
+#define rotate0(p,ang) ((p) *exp(complex<long double>(0,ang)))
+#define rotate(p,ang,about) (rotate0((p - about),ang)+ about)
+#define reflect0(v,m) (conj((v)/(m))*(m))
+#define reflect(p,p0,p1) conj((p - p0)/(p1-p0))*(p1-p0)  + p0   // Reflect point p1 around p0p1
+const long double PI= 3.141592653589793238462643383279;
+using namespace std;
+
+ll mod =  1e9 + 13;
+
+ll max(ll one, ll two)
+{
+    if(one > two)
+        return one;
+    return two;
+}
+ll min(ll one, ll two)
+{
+    if(one < two)
+        return one;
+    return two;
+}
+ll gcd(ll one, ll two)
+{
+    if(!two)
+        return one;
+    return gcd(two, one%two);
+}
+ll lcm(ll one, ll two)
+{
+    ll g = gcd(one, two);
+    return (one * two)/g;
+}
+ll power(ll one, ll two)
+{
+    if(!two)
+        return 1;
+    ll temp = power(one, two/2);
+    temp *= temp;
+    temp %= mod;
+    if(two%2)
+        temp *= one;
+    return temp%mod;
+}
+ll n,k;
+vector<ll> arr;
+vector<ll> dp;
+ll fun(ll indx =0)
+{
+    if(indx >=n -1)
+        return 0;
+    if(dp[indx] != -1)
+        return dp[indx];
+    ll mn = OO;
+    Loop(i,k+1)
+    {
+        if(i + indx == n)
+            break;
+        mn =min(mn, fun(indx+i) + abs(arr[indx] - arr[i + indx]));
+    }
+    return dp[indx] = mn;
+}
+
+void solve()
+{
+    cin>>n>>k;
+    arr = vector<ll>(n);
+    dp = vector<ll> (n,-1);
+    loop(i,n)
+        cin>>arr[i];
+    cout<<fun();
+}
+
+ 
+int main()
+{
+    speed;
+    //tests
+        solve();
+}  
