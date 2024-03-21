e9bc7fbae66c411a06a588ad32747096951083e8
إبراهيم <93207426+othmaine@users.noreply.github.com>
2024-03-21 08:37:15 +0200

    Committing files

---
+++ Atcoder/Python (PyPy 3.10-v7.3.12)/abc284_b/45554363.py
@@ -0,0 +1,11 @@
+t = int(input())
+for T in range(t):
+    n = int(input())
+    cnt = 0;
+    arr=(input().split())
+    for i in range(n):
+        x = int(arr[i]);
+        if x%2 == 1:
+            cnt += 1
+    print(cnt)
+
