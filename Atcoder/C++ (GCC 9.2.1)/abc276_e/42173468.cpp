7c13872527f0446150e3a4ac13028f6f5161ae4a
إبراهيم <93207426+othmaine@users.noreply.github.com>
2024-03-21 08:38:13 +0200

    Committing files

---
+++ Atcoder/C++ (GCC 9.2.1)/abc276_e/42173468.cpp
@@ -0,0 +1,131 @@
+#include <bits/stdc++.h>
+#define loop(i, n) for(ll i = 0; i< n; i++)
+#define Loop(i, n) for(ll i = 1; i< n; i++)
+#define ll long long
+ 
+#define fillArray(n, arr) ll arr[n]; for(ll i=0; i< n; i++) cin>>arr[i]
+#define fillVector(n, arr) vector<ll> arr(n);loop(i,n)cin>>arr[i];
+#define speed cin.tie(0); cin.sync_with_stdio(0)
+#define tests  ll t;cin>>t;Loop(T,t+1)
+#define OO 9223372036854775807//9223372036854775807
+#define dotProduct(a,b) ((conj(a)*(b)).real())
+#define crossProduct(a,b) ((conj(a)*b).imag())
+#define rotate0(p,ang) ((p) *exp(complex<long double>(0,ang)))
+#define rotate(p,ang,about) (rotate0((p - about),ang)+ about)
+#define reflect0(v,m) (conj((v)/(m))*(m))
+#define reflect(p,p0,p1) conj((p - p0)/(p1-p0))*(p1-p0)  + p0   // Reflect poll p1 around p0p1
+const long double PI= 3.141592653589793238462643383279;
+using namespace std;
+ll mod =  998244353;
+ 
+ll max(ll one, ll two)
+{
+    if(one > two)
+        return one;
+    return two;
+}
+ll min(ll one, ll two)
+{
+    if(one < two)
+        return one;
+    return two;
+}
+ll gcd(ll one, ll two)
+{
+    if(!two)
+        return one;
+    return gcd(two, one%two);
+}
+
+ll lcm(ll one, ll two)
+{
+    ll g = gcd(one, two);
+    return (one * two)/g;
+}
+ll len(ll x)
+{
+    if(!x)
+        return 0;
+    return 1 + len(x/10);
+}
+
+ll power(ll one, ll two)
+{
+    if(!two)
+        return 1;
+    ll temp = power(one, two/2);
+    temp *= temp;
+    temp %= mod;
+    if(two%2)
+        temp *= one%mod;
+    return temp%mod;
+}
+
+
+ll n,m;
+bool valid(ll x,ll y)
+{
+    return x>=0 &&  x< n && y>=0 && y<m;
+}
+
+vector<ll> dx = {0,0,1,-1};
+vector<ll> dy = {1,-1,0,0};
+
+vector<vector<bool>> visited;
+vector<vector<char>> arr;
+bool dfs(ll x, ll y, ll cnt)
+{
+    if(!valid(x,y))
+        return 0;
+    if(arr[x][y] == '#')
+        return 0;
+    if(cnt && arr[x][y] == 'S')
+    {
+        return cnt >=4;
+    }
+    if(visited[x][y])
+        return 0;
+    visited[x][y] = 1;
+    loop(i,4)
+    {
+        if(dfs(x + dx[i] , y+ dy[i] , cnt+1))
+            return 1;
+    }
+    return 0;
+}
+
+void solve()
+{
+    cin>>n>>m;
+    arr = vector<vector<char>> (n, vector<char>(m));
+    visited = vector<vector<bool>> (n, vector<bool>(m));
+    
+
+    ll x , y;
+    loop(i,n)
+    {
+        loop(j,m)
+        {
+            cin>>arr[i][j];
+            {
+                if(arr[i][j] == 'S')
+                {
+                    x = i,y = j;
+                }
+            }
+        }
+    }
+    if(dfs(x,y,0))
+    {
+        cout<<"Yes\n";
+    }
+    else
+        cout<<"No\n";
+}
+
+int main()
+{
+    speed;
+        solve();
+}
+
