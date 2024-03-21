90a1629be31a76f9c58994d62c4e3416f2f0137c
إبراهيم <93207426+othmaine@users.noreply.github.com>
2024-03-21 08:36:53 +0200

    Committing files

---
+++ Atcoder/C++ 20 (gcc 12.2)/abc322_a/46058366.cpp
@@ -0,0 +1,84 @@
+#include <bits/stdc++.h>
+#define loop(i, n) for(ll i = 0; i< n; i++)
+#define Loop(i, n) for(ll i = 1; i< n; i++)
+#define ll long long
+#define dd long double
+#define fillArray(n, arr) ll arr[n]; for(ll i=0; i< n; i++) cin>>arr[i]
+#define fillVector(n, arr) vector<ll> arr(n);loop(i,n)cin>>arr[i];
+#define speed cin.tie(0); cin.sync_with_stdio(0)
+#define tests  ll t;cin>>t;Loop(T,t+1)
+#define OO 1223372036854775807 //9223372036854775807
+#define dotProduct(a,b) ((conj(a)*(b)).real())
+#define crossProduct(a,b) ((conj(a)*b).imag())
+#define rotate0(p,ang) ((p) *exp(complex<long double>(0,ang)))
+#define rotate(p,ang,about) (rotate0((p - about),ang)+ about)
+#define reflect0(v,m) (conj((v)/(m))*(m))
+#define reflect(p,p0,p1) conj((p - p0)/(p1-p0))*(p1-p0)  + p0   // Reflect poll p1 around p0p1
+const long double PI= 3.141592653589793238462643383279;
+using namespace std;
+
+ll mod =998244353;
+
+ll max(ll one, ll two)
+{
+    if(one > two)
+        return one;
+    return two;
+}
+ll min(ll one, ll two)
+{
+    if(one < two)
+        return one;
+    return two;
+}
+ll gcd(ll one, ll two)
+{
+    if(!two)
+        return one;
+    return gcd(two, one%two);
+}
+ll lcm(ll one, ll two)
+{
+    ll g = gcd(one, two);
+    return (one * two)/g;
+}
+ll power(ll one, ll two)
+{
+    if(!two)
+        return 1;
+    ll temp = power(one, two/2);
+    temp *= temp;
+    temp %= mod;
+    if(two%2)
+        temp *= one;
+    return temp%mod;
+}
+vector<ll> fact;
+ll N = 2e5+1;
+void solve()
+{
+    ll n;
+    cin>>n;
+    string s;
+    cin>>s;
+    loop(i,n-2)
+    {
+        if(s[i] == 'A' && s[i+1]== 'B' && s[i+2] == 'C') 
+        {
+            cout<<i+1<<"\n";
+            return;
+        }
+    }
+    cout<<"-1";
+}
+
+
+
+ 
+int main()
+{
+    speed;
+    //tests
+        solve();
+}  
+
