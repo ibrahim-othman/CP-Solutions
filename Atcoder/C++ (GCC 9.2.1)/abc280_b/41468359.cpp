6add5023956109d4b3a1f6b78b94a813afb3d92d
إبراهيم <93207426+othmaine@users.noreply.github.com>
2024-03-21 08:38:57 +0200

    Committing files

---
+++ Atcoder/C++ (GCC 9.2.1)/abc280_b/41468359.cpp
@@ -0,0 +1,81 @@
+
+#include <iostream>
+#include <string>
+#include <cmath>
+#include <complex>
+#include <vector>
+#include <set>
+#include <stack>
+#include <queue>
+#include <map>
+#include <unordered_map>
+#include <unordered_set>
+#include <algorithm>
+#include <iomanip>
+#include <cstdlib>
+#include <complex>
+#include <numeric>
+#include <stdio.h>
+#include <stdlib.h>
+#define ll long long
+#define loop(i, n) for(ll i = 0; i< n; i++)
+#define Loop(i, n) for(ll i = 1; i< n; i++)
+ 
+#define fillArray(n, arr) ll arr[n]; for(ll i=0; i< n; i++) cin>>arr[i]
+#define fillVector(n, arr) vector<ll> arr(n);loop(i,n)cin>>arr[i];
+#define speed cin.tie(0); cin.sync_with_stdio(0)
+#define tests  ll t;cin>>t;Loop(T,t+1)
+#define OO 1000000100//9223372036854775807
+#define dotProduct(a,b) ((conj(a)*(b)).real())
+#define crossProduct(a,b) ((conj(a)*b).imag())
+#define rotate0(p,ang) ((p) *exp(complex<long double>(0,ang)))
+#define rotate(p,ang,about) (rotate0((p - about),ang)+ about)
+#define reflect0(v,m) (conj((v)/(m))*(m))
+#define reflect(p,p0,p1) conj((p - p0)/(p1-p0))*(p1-p0)  + p0   // Reflect point p1 around p0p1
+const long double PI= 3.141592653589793238462643383279;
+using namespace std;
+ 
+ll max(ll one, ll two)
+{
+    if(one > two)
+        return one;
+    return two;
+}
+ll min(ll one, ll two)
+{
+    if(one < two)
+        return one;
+    return two;
+}
+
+ll getLength(ll x)
+{
+    if(!x)
+        return 0;
+    return 1 + getLength(x/10);
+}
+
+void solve()
+{
+    ll n; cin >> n;
+    vector<ll> arr(n);
+    loop(i, n) {
+        cin >> arr[i];
+    }
+    ll sum = 0;
+    cout << arr[0] << ' ';
+    Loop(i, n) {
+        cout << arr[i] - arr[i-1] << ' ';
+    }
+}
+ 
+int main()
+{
+    //freopen("business.in", "r", stdin);
+    speed;
+    //tests
+    {
+        solve();
+    }
+}
+
