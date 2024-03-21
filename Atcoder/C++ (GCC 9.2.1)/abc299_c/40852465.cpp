34a03d7287b55e85e69147e17d9df7bdb47d5ff7
إبراهيم <93207426+othmaine@users.noreply.github.com>
2024-03-21 08:39:20 +0200

    Committing files

---
+++ Atcoder/C++ (GCC 9.2.1)/abc299_c/40852465.cpp
@@ -0,0 +1,99 @@
+#include <iostream>
+#include <string>
+#include <cmath>
+#include <complex>
+#include <vector>
+#include <set>
+#include <stack>
+#include <queue>
+#include <map>
+#include <algorithm>
+#include <iomanip>
+#include <cstdlib>
+#include <complex>
+#include <cmath>
+#include <unordered_map>
+#include <unordered_set>
+#define loop(i, n) for(ll i = 0; i< n; i++)
+#define Loop(i, n) for(ll i = 1; i< n; i++)
+#define ll long long
+ 
+#define fillArray(n, arr) ll arr[n]; for(ll i=0; i< n; i++) cin>>arr[i]
+#define speed cin.tie(0); cin.sync_with_stdio(0)
+#define tests  ll t;cin>>t;Loop(T,t+1)
+#define OO 9203372036854775807
+#define dotProduct(a,b) ((conj(a)*(b)).real())
+#define crossProduct(a,b) ((conj(a)*b).imag())
+#define rotate0(p,ang) ((p) *exp(complex<double>(0,ang)))
+#define rotate(p,ang,about) (rotate0((p - about),ang)+ about)
+#define reflect0(v,m) (conj((v)/(m))*(m))
+#define reflect(p,p0,p1) conj((p - p0)/(p1-p0))*(p1-p0)  + p0   // Reflect point p1 around p0p1
+const long double pi= 3.141592653589793238462643383279;
+ll mod =  998244353;
+const long double eps= 1e-8;
+using namespace std;
+
+
+ll max(ll one, ll two)
+{
+    if(two > one)
+        return two;
+    return one;
+}
+
+ll min(ll one, ll two)
+{
+    if(two < one)
+        return two;
+    return one;
+}
+
+ll power(ll one , ll two)
+{
+    if(!two)
+        return 1;
+    ll temp = power(one, two/2);
+    temp %= mod;
+    temp *= temp;
+    temp %= mod;
+    if(two%2)
+        temp *= one;
+    return temp%mod;
+}
+void solve()
+{
+    int n;
+    cin>>n;
+    string s;
+    cin>>s;
+    if(s.find('-') ==string::npos )
+    {
+        cout<<"-1";
+        return;
+    }
+    int mx = 0;
+    int cnt = 0;
+    for(auto x:s)
+    {
+        if(x != 'o')
+        {
+            mx = max(cnt,mx);
+            cnt = 0;
+        }
+        else
+            cnt++;
+    }
+    mx = max(mx, cnt);
+    if(mx == 0)
+        mx = -1;
+    cout<<mx;
+}
+int main()
+{
+    speed;
+    {
+        solve();
+    }
+}
+
+
